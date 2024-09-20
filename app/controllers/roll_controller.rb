1c82e05d84b142c4cb7c924ea993c067390fe791
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-20 17:50:27 +0000

    finished templates for home page and rolls page

--- app/controllers/roll_controller.rb
+++ app/controllers/roll_controller.rb
@@ -1,27 +1,21 @@
 class RollController < ApplicationController
 
-
-@num_dice=params.fetch("number_of_dice")
-@sides=params.fetch("sides")
-@rolls=[]
-
-@num_dice.do 
-  one_roll=rand(1..@sides)
-  @rolls.push(one_roll)
-
-end
-
-
   def go_home
-
-
+    render(:template=> "roll_templates/home_template")
   end
 
 
   def roll_dice
 
+    @num_dice=params.fetch("number_of_dice")
+    @sides=params.fetch("sides")
+    @rolls=[]
 
+    @num_dice.do 
+      one_roll=rand(1..@sides)
+      @rolls.push(one_roll)
+    end
+    render(:template=> "roll_templates/roll_template")
   end
-
 
 end
